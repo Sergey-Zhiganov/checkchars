3229aafa4e68a53ed323cd31a3562d2e51582faa
SergeyZhiganov <isip_s.a.zhiganov@mpt.ru>
2024-06-07 12:34:41 +0300

    Библиотека

---
+++ CheckChars/CheckChars.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+using namespace std;
+
+typedef bool (*CheckCharsInStringFunc)(const char*, const char*);
+
+
+int main()
+{
+    SetConsoleCP(1251);
+    setlocale(LC_ALL, "ru");
+    string inputString;
+    string inputChars;
+    cout << "Введите строку: ";
+    getline(cin, inputString);
+    cout << "Введите символы для поиска: ";
+    cin >> inputChars;
+    HMODULE hDLL = LoadLibraryA("CheckCharsDLL.dll");
+    if (!hDLL) {
+        cout << "Не удалось загрузить библиотеку!" << endl;
+        return 1;
+    }
+    CheckCharsInStringFunc checkCharsInString = (CheckCharsInStringFunc)GetProcAddress(hDLL, "CheckCharsInString");
+    if (!checkCharsInString) {
+        cout << "Не удалось получить адрес функции!" << endl;
+        FreeLibrary(hDLL);
+        return 1;
+    }
+    bool result = checkCharsInString(inputString.c_str(), inputChars.c_str());
+    if (result) {
+        cout << "Все символы найдены в строке" << endl;
+    }
+    else {
+        cout << "Не все символы найдены в строке" << endl;
+    }
+    FreeLibrary(hDLL);
+    return 0;
+}
---
+++ CheckCharsDLL/dllmain.cpp
@@ -0,0 +1,18 @@
+﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
+#include "pch.h"
+#include <windows.h>
+#include <cstring>
+
+extern "C" __declspec(dllexport) bool CheckCharsInString(const char* str, const char* chars) {
+	while (*chars) {
+		if (!strchr(str, *chars)) {
+			return false;
+		}
+		chars++;
+	}
+	return true;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
+	return TRUE;
+}
